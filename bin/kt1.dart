dae471661c4a9dff404572081f5be77d6e41abbd
Никита <ilyinnikita90@mail.ru>
2023-10-07 17:59:50 +0300

    second

--- bin/kt1.dart
+++ bin/kt1.dart
@@ -1,2 +1,20 @@
-import 'package:kt1/kt1.dart' as kt1;
+import 'dart:io';
 
+import 'package:kt1/kt1.dart' as kt1;
+import 'package:dio/dio.dart';
+
+void main(List<String> arguments) async{
+Dio client = Dio();
+String url = 'https://dummyjson.com/products';
+var responcse = await client.get(url);
+Map<String,dynamic> data = responcse.data;
+
+int count = 0;
+for(var el in data['products']){
+  if (el['price']<1000){
+count++;
+}
+}
+print(count);
+
+}
